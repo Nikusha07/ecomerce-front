86e7a11eb0d70c2582af0df033d4e8f48620bb8b
Nikusha07 <129980489+Nikusha07@users.noreply.github.com>
2024-04-28 22:27:57 +0400

    add header responsive

--- components/Heder.js
+++ components/Heder.js
@@ -1,27 +1,45 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        setWindowWidth(window.innerWidth);
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
-        <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-            <Link className="roboto-light-italic" href="/">
-                <span className="text-2xl font-bold cursor-pointer hover:text-red-500 transition duration-300">
-                    Ecommerce
-                </span>
-            </Link>
-            <nav className="flex gap-4">
-                <Link href="/">
-                    <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">Home</span>
+        <header className="bg-gray-800 text-white w-full p-4 flex justify-between items-center">
+                <Link className="roboto-light-italic" href="/">
+                    <span className="text-2xl font-bold cursor-pointer hover:text-red-500 transition duration-300">
+                        Ecommerce
+                    </span>
                 </Link>
-                <Link href="/products">
-                    <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">All Products</span>
-                </Link>
-                <Link href="/cart">
-                    <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">Cart</span>
-                </Link>
-                <Link href="/account">
-                    <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">Account</span>
-                </Link>
-            </nav>
+            {windowWidth > 630 && (
+             <nav className="flex gap-4">
+             <Link href="/">
+                 <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">Home</span>
+             </Link>
+             <Link href="/products">
+                 <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">All Products</span>
+             </Link>
+             <Link href="/cart">
+                 <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">Cart</span>
+             </Link>
+             <Link href="/account">
+                 <span className="roboto-thin-italic cursor-pointer hover:text-red-500 transition duration-300">Account</span>
+             </Link>
+         </nav>
+            )}
+           
         </header>
     );
 };
